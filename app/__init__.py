fa78c268e385de919edf36c15bd8ecdc8a613470
kyo <odinxp@gmail.com>
2019-03-13 21:20:05 +0800

    使用了flask-bootstrap base.html的煩人版本，將廢棄

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -9,3 +11,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
