6db3da004d8e78d6cd7e3f7857f28d939365fe98
kyo <odinxp@gmail.com>
2019-03-15 16:20:54 +0800

    增加SECRET_KEY，強化表單資料安全性 引入session使用，讓使者用在送出帳密資料後，可以在轉回首頁後看到帶有帳號名的個人歡迎 session讓POST資料可以跨request使用

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,session
 # from flask_bootstrap import Bootstrap
 from .forms import LoginForm
 
 app = Flask(__name__)
 # bootstrap=Bootstrap(app)
-
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
 @app.route('/')
 def index():
+    if session.get('name'):
+        return render_template('index.html',name=session.get('name'))
     return render_template('index.html')
 
 @app.route('/user/<name>')
@@ -18,8 +20,12 @@ def user(name):
 @app.route('/login', methods=['GET','POST'])
 def login():
     form=LoginForm(csrf_enabled=False)
+    # form=LoginForm()
     msg=f'帳號={form.username.data},密碼={form.password.data}'
     print(msg)
+    
+    session['name']=form.username.data
+
     if form.validate_on_submit():
         return redirect(url_for('index'))
     return render_template('login.html',form=form)
