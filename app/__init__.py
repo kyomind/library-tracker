53ae229f542477bb28586eeeec8a289c1ac23876
kyo <odinxp@gmail.com>
2019-03-15 16:22:33 +0800

    Revert "增加SECRET_KEY，強化表單資料安全性"
    This reverts commit 6db3da004d8e78d6cd7e3f7857f28d939365fe98.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
-from flask import Flask,render_template,redirect,url_for,session
+from flask import Flask,render_template,redirect,url_for
 # from flask_bootstrap import Bootstrap
 from .forms import LoginForm
 
 app = Flask(__name__)
 # bootstrap=Bootstrap(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 
 @app.route('/')
 def index():
-    if session.get('name'):
-        return render_template('index.html',name=session.get('name'))
     return render_template('index.html')
 
 @app.route('/user/<name>')
@@ -20,12 +18,8 @@ def user(name):
 @app.route('/login', methods=['GET','POST'])
 def login():
     form=LoginForm(csrf_enabled=False)
-    # form=LoginForm()
     msg=f'帳號={form.username.data},密碼={form.password.data}'
     print(msg)
-    
-    session['name']=form.username.data
-
     if form.validate_on_submit():
         return redirect(url_for('index'))
     return render_template('login.html',form=form)
