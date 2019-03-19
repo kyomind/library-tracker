edbb85586e6e35b3fb0fab32253366478b070f8b
kyo <odinxp@gmail.com>
2019-03-19 22:09:32 +0800

    先flask db upgrade建立資料表欄位，以使用新路徑資料庫

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_DATABASE_URI = \
-        'sqlite:///'+ os.path.join(basedir,'app/tracker.db')
+        'sqlite:///'+ os.path.join(basedir,'tracker.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
