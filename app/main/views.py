c5b2018476ef9b64b18dfc5f4335da56df515187
kyo <odinxp@gmail.com>
2019-03-22 20:40:42 +0800

    修改首頁的路由與模版，開始把圖書表格加入

--- app/main/views.py
+++ app/main/views.py
@@ -8,9 +8,8 @@ from datetime import timedelta
 
 @main.route('/')
 def index():
-    if session.get('name'):
-        return render_template('index.html',name=session.get('name'))
-    return render_template('index.html')
+    books=['aa','bb']
+    return render_template('index.html',books=books)
 
 @main.route('/user/<name>')
 def user(name):
@@ -18,22 +17,3 @@ def user(name):
     time= join_time+timedelta(hours=8)
     time= time.strftime("%Y-%m-%d")
     return render_template('user.html',name=name,time=time)
-
-@main.route('/login', methods=['GET','POST'])
-def login():
-    form=LoginForm()
-
-    if form.validate_on_submit():
-        exist_user=User.query.filter_by(username=
-        form.username.data).first()
-
-        if not exist_user:
-            new_user=User(username=form.username.data)
-            new_user.set_password(form.password.data)
-
-            db.session.add(new_user)
-            db.session.commit()
-
-        session['name']=form.username.data
-        return redirect(url_for('main.index'))
-    return render_template('login.html',form=form)
