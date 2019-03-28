4d5eef7ee5b7c5ff892f7ae1546cef56b1724217
kyo <odinxp@gmail.com>
2019-03-28 12:16:27 +0800

    新增發信功能email.py內容，先採用肖鵬的簡單做法，不加入template渲染

--- app/email.py
+++ app/email.py
@@ -1,4 +1,21 @@
 from threading import Thread
-from flask import current_app
+from flask import current_app,render_template
 from flask_mail import Message
 from . import mail
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, recipients, text_body, html_body):
+    msg = Message(
+        subject=subject,
+        recipients=recipients,
+        reply_to='odinxp@gmail.com'
+    )
+    msg.body = text_body
+    msg.html = html_body
+    thr=Thread(target=send_async_email,
+                args=(current_app._get_current_object(), msg))
+        
+    thr.start()
