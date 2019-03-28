0415a0c4d58b0d5cb2f89ce7815736ce1bb6d359
kyo <odinxp@gmail.com>
2019-03-28 11:26:10 +0800

    加入email的環境變數至config

--- config.py
+++ config.py
@@ -6,5 +6,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = \
         'sqlite:///'+ os.path.join(basedir,'tracker.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-      
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
