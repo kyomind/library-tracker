870a55c97bfc55a67d2588af7ab339a3cf7c5401
kyo <odinxp@gmail.com>
2019-04-02 15:58:39 +0800

    新增500、404網頁之錯誤訊息 新增登出確認提醒

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,8 +3,8 @@ from . import main
 
 @main.app_errorhandler(404)
 def not_found(err):
-    return render_template('404.html'),404
+    return render_template('404.html', err=err),404
 
 @main.app_errorhandler(500)
 def server_error(err):
-    return render_template('500.html'),500
+    return render_template('500.html', err=err),500
