4aed214a5cef22986be7970dc0fa9d7960855a23
kyo <odinxp@gmail.com>
2019-04-13 00:11:01 +0800

    新增申請ssl路由與檔案

--- app/main/views.py
+++ app/main/views.py
@@ -130,3 +130,8 @@ def user(name):
         return redirect(url_for('main.user',name=name))
 
     return render_template('user.html',name=name,time=time,form=form,count=count)
+
+# ssl for free 申請用路由
+@main.route('.well-known/acme-challenge/TV09JIit5bRxAwpVqj1dKDAvDptXGogWch5HlP_C3JQ')
+def ssl():
+    return redirect(url_for('static', filename='TV09JIit5bRxAwpVqj1dKDAvDptXGogWch5HlP_C3JQ'))
