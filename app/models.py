fd35999feb4c05f24513e493ebf7dfdd7b79183a
kyo <odinxp@gmail.com>
2019-04-27 14:20:04 +0800

    變更註冊頁面說明文字

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class User(UserMixin, db.Model):
         return f'user {self.username}'
 
     def set_password(self, password):
-        self.password= generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password,password)
